d70a71512fc20a930b8ad1e5d87b043fa8776dfd
Rafli Dewanto <dewantorafli@gmail.com>
2025-04-12 09:06:30 +0700

    Initial Commit

---
+++ application/models/M_welcome.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No directed script access allowed');
+
+class M_welcome extends CI_Model{
+    
+  
+  public function create($id, $filename){
+        $data = [
+            'id' => $id,
+            'name' =>$this->input->post('name', TRUE),
+            'description' =>$this->input->post('description', TRUE),
+            'filename'=> $filename
+        ];
+        $this->db->insert('post', $data);
+    }
+    
+}
+
+?>
